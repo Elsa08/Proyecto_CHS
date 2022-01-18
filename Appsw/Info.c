7f1ad87072e471d1328340a13296343fcae526fe
DavidMDCV <91056298+DavidMDCV@users.noreply.github.com>
2022-01-18 17:15:23 +0100

    Añadido distribución de carpetas y ficheros de información.

---
+++ Appsw/Info.c
@@ -0,0 +1,6 @@
+// Distribución de archivos y directorios.
+// Appsw: incluye la aplicación principal que hará que se ejecute el programa
+// Basesw: incluye las funciones necesarias para que funcione la aplicación principal
+// Basesw/inc: includes .h necesarios
+// Basew/src: software .c con las funciones a utilizar.
+
